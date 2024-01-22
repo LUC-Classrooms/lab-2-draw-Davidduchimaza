d6851810ac524b5338c9bf4064cc8b89a0910357
Davidduchimaza <151409646+Davidduchimaza@users.noreply.github.com>
2024-01-22 14:20:28 -0600

    Update script.js

--- script.js
+++ script.js
@@ -4,19 +4,9 @@ function setup(){
 
 function draw(){
   background(200);
-  ellipse(50,50,80,80)
+  stroke(0);
+  fill(255,0,0);
+  ellipse(50,50,80,80);
  
-  
-  ellipse(80,80,100,100)
-  
-  noStroke();
-let c = color(50, 55, 100);
-fill(c);
-rect(0, 10, 45, 80);
-colorMode(HSB, 100);
-c = color(50, 55, 100);
-fill(c);
-rect(55, 10, 45, 80);
-
 
 }
