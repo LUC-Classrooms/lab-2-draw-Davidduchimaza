ce40d4d08fc91be48b6c96a0f5fcbdeb23870a2a
Davidduchimaza <151409646+Davidduchimaza@users.noreply.github.com>
2024-01-22 10:41:34 -0600

    Update script.js

--- script.js
+++ script.js
@@ -5,8 +5,15 @@ function setup(){
 function draw(){
   background(200);
   // 
-  fill("blue") ;
+  fill("light blue") ;
   rec(50,50,100,100) ;
+
+  fill("yellow") ;
+  ellipse(20,20,50,50) ;
+
+  fill("red") ;
+  triangle((150, 300, 300, 200, 450, 300) ;
+  
   
 
   
